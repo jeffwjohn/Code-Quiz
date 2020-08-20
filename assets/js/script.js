541c81225da6b5fafac71d35d051ff7e26fc200b
Jeff Johnston <jeffwjohn@yahoo.com>
2020-08-20 13:17:09 -0500

    add timer quiz complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,35 +5,40 @@ var choiceButton = document.querySelector(".choice-btn");
 var startButton = document.querySelector("#start");
 // var now = new Date().getTime();
 
-function startTimer () {
-var timeLimit = new Date().getTime() + 60000;
-// Set the date we're counting down to
-var countDownDate = new Date(timeLimit).getTime();
+function startTimer() {
+    var timeLimit = new Date().getTime() + 60000;
+    // Set the date we're counting down to
+    var countDownDate = new Date(timeLimit).getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function () {
+    // Update the count down every 1 second
+    var x = setInterval(function () {
 
-    // Get today's date and time
-    var now = new Date().getTime();
+                // Get today's date and time
+                var now = new Date().getTime();
 
-    // Find the distance between now and the count down date
-    var distance = countDownDate - now;
+                // Find the distance between now and the count down date
+                var distance = countDownDate - now;
 
-    // Time calculations for days, hours, minutes and seconds
-    // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                // Time calculations for days, hours, minutes and seconds
+                // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    // Display the result in the element with id="timer"
-    document.getElementById("timer").innerHTML = "Time: " + seconds + "s ";
+                // Display the result in the element with id="timer"
+                document.getElementById("timer").innerHTML = "Time: " + seconds + "s ";
 
-    // If the count down is finished, write some text
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("timer").innerHTML = "TIME UP!";
-    }
-}, 1000);
+                // If the count down is finished, write some text
+                if (distance < 0) {
+                    clearInterval(x);
+                    document.getElementById("timer").innerHTML = "TIME UP!";
+                } else if (quiz.isEnded()) {
+                    clearInterval(x);
+                    document.getElementById("timer").innerHTML = "QUIZ COMPLETE!";
+                }
+            
+        
+            },1000)
 }
 // //var choiceButton = document.querySelectorAll(".choice-btn");
 // var buttons = document.getElementsByTagName("button");
@@ -174,7 +179,7 @@ Quiz.prototype.guess = function (answer) {
     if (this.getQuestionIndex().isCorrectAnswer(answer)) {
         this.score++;
     }
-    
+
     this.questionIndex++;
 }
 
