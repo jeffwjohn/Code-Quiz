3496bc7f6b079c608aa8f31eae83d3684fb60a58
Jeff Johnston <jeffwjohn@yahoo.com>
2020-08-20 14:15:53 -0500

    enable timer penalty

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,41 +7,17 @@ var seconds = 60;
 // var now = new Date().getTime();
 
 function startTimer() {
-    var timeLimit = new Date().getTime() + 60000;
-    // Set the date we're counting down to
-    var countDownDate = new Date(timeLimit).getTime();
-
-    // Update the count down every 1 second
     var x = setInterval(function () {
-
-        // Get today's date and time
-        var now = new Date().getTime();
-
-        // Find the distance between now and the count down date
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        // Display the result in the element with id="timer"
+        seconds--;
         document.getElementById("timer").innerHTML = "Time: " + seconds + "s ";
-
         // If the count down is finished, write some text
-        if (distance < 0) {
+        if (seconds < 0) {
             clearInterval(x);
             document.getElementById("timer").innerHTML = "Time Up!";
         } else if (quiz.isEnded()) {
             clearInterval(x);
             document.getElementById("timer").innerHTML = "Quiz Complete!";
         };
-        if (question.isCorrectAnswer()) {
-            distance = countDownDate - now - 10000;
-        }
-
-
     }, 1000)
 }
 // //var choiceButton = document.querySelectorAll(".choice-btn");
