f6a69026235711a2de8b341869d4b688a3b017ce
Jeff Johnston <jeffwjohn@yahoo.com>
2020-08-21 12:15:57 -0500

    clean code

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,11 @@ var questionCard = document.querySelector(".question");
 var choiceButton = document.querySelector(".choice-btn");
 var startButton = document.querySelector("#start");
 var seconds = 60;
-// var now = new Date().getTime();
 
 function startTimer() {
     var x = setInterval(function () {
         seconds--;
         document.getElementById("timer").innerHTML = "Time: " + seconds + "s ";
-        // If the count down is finished, write some text
         if (seconds < 0) {
             clearInterval(x);
             document.getElementById("timer").innerHTML = "Time Up!";
@@ -20,131 +18,14 @@ function startTimer() {
         };
     }, 1000)
 }
-// //var choiceButton = document.querySelectorAll(".choice-btn");
-// var buttons = document.getElementsByTagName("button");
-// var quizObj = {
-//     question: "",
-//     choices: []
-// }
 
-// var Question = function(text, choices, answer) {
-//     this.text = text;
-//     this.choices = choices;
-//     this.answer = answer;
-// }
-
-// var questions = [
-//     new Question("The condition in an if/else statement is enclosed with __________.", ["quotes", "curly brackets", "parentheses", "square brackets"], "parentheses"),
-//     new Question("Commonly used data types DO NOT include:", ["strings", "booleans", "alerts", "numbers"], "alerts"),
-//     new Question("A very useful tool used during development and debugging for printing content to the debugger is:", ["JavaScript", "Terminal/Bash", "for loops", "console.log"], "console.log"),
-//     new Question("String values must be enclodsed within __________ when being assigned to variables.", ["commas", "curly brackets", "quotes", "parentheses"], "quotes"),
-//     new Question("Arrays in JavaScript can be used to store __________", ["numbers and strings", "other arrays", "booleans", "all of the above"], "all of the above")
-// ];
-
-// // var createCard = function () {
-// //     // create question 
-// //     var questionEl = document.createElement("article");
-// //     questionEl.className = "question";
-// //     questionEl.innerHTML = "<h2>The condition in an if / else statement is enclosed with ________.</h2>";
-
-// //     // create choices
-// //     var choice1El = document.createElement("button");
-// //     choice1El.textContent = "1. quotes";
-// //     choice1El.id = "choice1";
-// //     choice1El.className = "button choice-btn";
-// //     var choice2El = document.createElement("button");
-// //     choice2El.textContent = "2. curly brackets";
-// //     choice2El.id = "choice2";
-// //     choice2El.className = "button choice-btn";
-// //     var choice3El = document.createElement("button");
-// //     choice3El.textContent = "3. parentheses";
-// //     choice3El.id = "choice3";
-// //     choice3El.className = "button choice-btn correct";
-// //     choice3El.name = "correct";
-// //     var choice4El = document.createElement("button");
-// //     choice4El.textContent = "4. square brackets";
-// //     choice4El.id = "choice4";
-// //     choice4El.className = "button choice-btn";
-
-// //     questionEl.appendChild(choice1El);
-// //     questionEl.appendChild(choice2El);
-// //     questionEl.appendChild(choice3El);
-// //     questionEl.appendChild(choice4El);
-
-
-// //     mainEl.appendChild(questionEl);
-// // };
-// // choiceButton.onclick = function () {
-// //     alert("you clicked a choice");
-// // };
-
-// var initialize = function () {
-//     if (createCard.isEnded()) {
-//         showScores();
-//     }
-//     else {
-//         var element = document.getElementById("question");
-//         element.innerHTML = createCard.get
-//     }
-// }
-
-// var checkAnswer = function (e) {
-//     if (e.target.classList.contains('choice-btn') && e.target.classList.contains('correct')) {
-//         var rightEl = document.createElement("div");
-//         rightEl.textContent = "Right!";
-//         rightEl.className = "verify";
-//         mainEl.appendChild(rightEl);
-//         var nextEl = document.createElement("button");
-//         nextEl.textContent = "Next";
-//         nextEl.id = "next";
-//         nextEl.className = "button";
-//         mainEl.appendChild(nextEl);
-//         console.log("RIGHT!");
-
-//     } else if (e.target.id === 'start' || e.target.id === 'next') {
-//         console.log("QUIZ BEGINS!");
-
-//     } else {
-//         var wrongEl = document.createElement("div");
-//         wrongEl.textContent = "Wrong!";
-//         wrongEl.className = "verify";
-//         mainEl.appendChild(wrongEl);
-//         console.log("WRONG!");
-//     }
-// };
-
-// var gameOver = function () {
-
-// };
-
-// var highScore = function () {
-
-// };
-
-// // START QUIZ
+// START QUIZ
 var startQuiz = function (event) {
-    //var startCard = document.getElementById("#startCard");
     startCard.remove();
-    // createCard()
     populate()
     startTimer()
 };
 
-// // CREATE QUESTION CARD
-// var createCard = new Card(questions);
-
-// // SHOW QUIZ
-// initialize();
-
-// LISTENERS
-// startButton.addEventListener("click", startQuiz);
-
-//document.querySelector("#main").addEventListener('click', checkAnswer);
-
-// choiceButton.addEventListener("click", checkAnswer);
-// pageContentEl.addEventListener("click", taskButtonHandler);
-
-//startQuiz(e)
 function Quiz(questions) {
     this.score = 0;
     this.questions = questions;
@@ -180,14 +61,11 @@ Question.prototype.isCorrectAnswer = function (choice) {
     return this.answer === choice;
 }
 
-
 function populate() {
     if (quiz.isEnded()) {
         gameOver();
+        
     } else {
-        // show question
-        // var element = document.getElementById("question");
-        // element.innerHTML = quiz.getQuestionIndex().text;
 
         // create question 
         var questionEl = document.createElement("article");
@@ -196,7 +74,7 @@ function populate() {
         mainEl.appendChild(questionEl);
 
 
-        // show options
+        // create choice buttons
         var choices = quiz.getQuestionIndex().choices;
         for (var i = 0; i < choices.length; i++) {
             var choiceEl = document.createElement("button");
@@ -210,9 +88,7 @@ function populate() {
             element.innerHTML = choices[i];
             guess("btn" + i, choices[i]);
         }
-
         showProgress();
-
     }
 };
 
@@ -235,7 +111,6 @@ function guess(id, guess) {
     }
 };
 
-
 function showProgress() {
     var currentQuestionNumber = quiz.questionIndex + 1;
     var element = document.getElementById("progress");
@@ -250,7 +125,7 @@ var scoreEl = scores[0];
 var initialsInput = document.querySelector('#initials');
 
 var gameOver = function () {
-    
+
     var scoreCard = document.createElement("article");
     scoreCard.id = "score-card-id";
     scoreCard.className = "score-card";
@@ -260,45 +135,25 @@ var gameOver = function () {
     gameOverHTML += "<form><label for='initials'>Enter initials: </label><input type='text' id='initials' name='initials' /><br><button id='btn'>Submit</button></form>"
     scoreCard.innerHTML = gameOverHTML;
     scores.push(quiz.score);
-    console.log(scores);
     var button = document.getElementById('btn');
     button.onclick = function () {
         event.preventDefault();
 
         var score = quiz.score
         var initials = document.querySelector('#initials').value;
-    
+
         if (initials === "") {
             alert("Initials field cannot be blank! Try again.");
-            
+
         } else {
             alert("Your score has been saved. Click on 'View High Scores' to see your rank!");
-    
+
             localStorage.setItem('score', score);
             localStorage.setItem('initials', initials);
         }
-        console.log(initials);
     }
-    
-
-    // scores.push({score:quiz.score, player:'#initials'});
 };
 
-
-
-
-
-
-
-
-// Quiz.score.forEach(scoreCompiler);
-
-// function scoreCompiler () {
-//     scores.push({score:score, player: initials});
-// }
-
-
-// create questions here
 var questions = [
     new Question("The condition in an if/else statement is enclosed with __________.", ["quotes", "curly brackets", "parentheses", "square brackets"], "parentheses"),
     new Question("Commonly used data types DO NOT include:", ["strings", "booleans", "alerts", "numbers"], "alerts"),
@@ -307,11 +162,6 @@ var questions = [
     new Question("Arrays in JavaScript can be used to store __________", ["numbers and strings", "other arrays", "booleans", "all of the above"], "all of the above")
 ];
 
-
-// create quiz
 var quiz = new Quiz(questions);
-var question = new Question();
-// display quiz
-startButton.addEventListener("click", startQuiz);
 
-//populate();
+startButton.addEventListener("click", startQuiz);
